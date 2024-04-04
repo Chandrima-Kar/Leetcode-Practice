c1febc8e40dcb18fae8bf2b206a8223782c1c6ee
Chandrima Kar <chandrimakar16@gmail.com>
2024-04-04 18:43:48 +0530

    1539-4th april

---
+++ 1539. Kth Missing Positive Number.java
@@ -0,0 +1,38 @@
+/*
+ * Given an array arr of positive integers sorted in a strictly increasing
+ * order, and an integer k.
+ * 
+ * Return the kth positive integer that is missing from this array.
+ * 
+ * 
+ * 
+ * Example 1:
+ * 
+ * Input: arr = [2,3,4,7,11], k = 5
+ * Output: 9
+ * Explanation: The missing positive integers are [1,5,6,8,9,10,12,13,...]. The
+ * 5th missing positive integer is 9.
+ * Example 2:
+ * 
+ * Input: arr = [1,2,3,4], k = 2
+ * Output: 6
+ * Explanation: The missing positive integers are [5,6,7,...]. The 2nd missing
+ * positive integer is 6.
+ */
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int low = 0, high = arr.length - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            int miss = arr[mid] - (mid + 1);
+            if (miss < k)
+                low = mid + 1;
+            else
+                high = mid - 1;
+        }
+        return (low + k);
+    }
+}
+
+// https://youtu.be/uZ0N_hZpyps?si=Hc7lI6zRB7Wa1GQ6
+// https://takeuforward.org/arrays/kth-missing-positive-number/
